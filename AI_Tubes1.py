c91b0f306b5108833177c7473dff316ba4983674
Naufal Malik <naufalmalikr@yahoo.co.id>
2016-10-02 06:15:17 +0700

    nambahin comment lebih lengkap

--- AI_Tubes1.py
+++ AI_Tubes1.py
@@ -1,16 +1,25 @@
 from __future__ import division
 import random
 
+"""
+variable : jadwal matkul
+domain : <ruang, jam, hari>
+constraint :
+1. ruangan yang sesuai (jika ada syarat ruangan tertentu di jadwal matkul)
+2. waktu (jam dan hari) yang sesuai dengan syarat jadwal matkul
+3. alldiff (tidak konflik di ruang, jam, dan hari yang sama)
+"""
+
 class Ruangan:
     def __init__(self, nama, jamBuka, jamTutup, hari):
         self.nama = nama
         self.jamBuka = int(jamBuka[:2])
         self.jamTutup = int(jamTutup[:2])
         self.hari = hari #list hari apa aja Ruangan buka
-        self.sel = [] #tabel projeksi antara jam dan hari (slot time), mempermudah menghitung konflik
+        self.sel = [] #tabel projeksi antara jam dan hari (slot time)
         for i in range(11):
             self.sel.append([[], [], [], [], []]) #tiap sel berisi list of Matkul
-        self.selAvailable = (self.jamTutup - self.jamBuka) * len(self.hari)
+        self.selAvailable = (self.jamTutup - self.jamBuka) * len(self.hari) #untuk persen keefektifan
 
     def slotPlus(self, matkul, hari, jamMulai, jamSelesai): #tambahkan matkul ke slot time
         for i in range(jamMulai, jamSelesai):
@@ -20,7 +29,7 @@ class Ruangan:
         for i in range(jamMulai, jamSelesai):
             self.sel[i - 7][hari - 1].remove(matkul) #dikurang 7 untuk jam, dikurang 1 untuk hari
 
-    def deleteAllSel(self):
+    def deleteAllSel(self): #semua sel di kosongin lagi
         for jam in self.sel:
             for hari in jam:
                 del hari[:]
@@ -30,7 +39,7 @@ class Ruangan:
         for i in range(11): #cek tiap sel
             if i + 7 >= self.jamBuka and i + 7 <= self.jamTutup:
                 for j in self.hari:
-                    if len(self.sel[i][int(j) - 1]) > 0: #ada isinya
+                    if len(self.sel[i][int(j) - 1]) > 0: #ada isinya, untuk persen keefektifan
                         hasil += 1
         return hasil
 
@@ -39,44 +48,44 @@ class Ruangan:
         for i in range(11): #cek tiap sel
             if i + 7 >= self.jamBuka and i + 7 <= self.jamTutup:
                 for j in self.hari:
-                    if len(self.sel[i][int(j) - 1]) == 1: #tidak konflik
+                    if len(self.sel[i][int(j) - 1]) == 1: #tidak konflik, sebagai fitness function
                         hasil += 1
         return hasil
 
 class Matkul:
     def __init__(self, nama, jamBuka, jamTutup, sks, hari):
         self.nama = nama
-        self.jamBuka = int(jamBuka[:2])
+        self.jamBuka = int(jamBuka[:2]) #BUKAN jamMulai
         self.jamTutup = int(jamTutup[:2])
         self.sks = int(sks[:2])
         self.hari = hari #list hari apa aja Matkul tersedia
 
-    def addListDomain(self, listDomain): #yang boleh mengakses listDomain dan idxDomain hanya class Matkul
+    def addListDomain(self, listDomain):
         self.__listDomain = listDomain
         self.nDomain = len(self.__listDomain) #banyaknya domain
 
-    def setIdxDomain(self, idxDomain): #untuk initialize random
+    def setIdxDomain(self, idxDomain): #pilih satu domain dari listDomain berdasarkan indexnya
         self.__idxDomain = idxDomain
         self.__addToSlot()
 
-    def getDomain(self):
+    def getDomain(self): #yang boleh mengakses listDomain dan idxDomain hanya class Matkul
         return self.__listDomain[self.__idxDomain]
 
-    def idxPlus(self): #ganti domain dengan menambah indexnya
+    def idxPlus(self): #ganti domain dengan menambah 1 index di listDomain nya
         self.__deleteFromSlot()
         self.__idxDomain += 1
         self.__idxDomain %= self.nDomain #supaya tidak out of bond
         self.__addToSlot()
 
-    def __addToSlot(self): #yang boleh menambah dan menghapus dari slot time hanya class Matkul
+    def __addToSlot(self): #yang boleh menambah ke slot time hanya class Matkul
         domain = self.getDomain()
         domain.ptrRuangan.slotPlus(self, domain.hari, domain.jamMulai, domain.jamSelesai)
 
-    def __deleteFromSlot(self):
+    def __deleteFromSlot(self): #yang boleh menghapus dari slot time hanya class Matkul
         domain = self.getDomain()
         domain.ptrRuangan.slotMinus(self, domain.hari, domain.jamMulai, domain.jamSelesai)
 
-    def printConsole(self): #print data-data ke console
+    def printConsole(self): #print data-data ke console, gaperlu dimengerti ini hiasan doang
         domain = self.getDomain()
         idxHari = domain.hari
         if idxHari == 1:
@@ -92,6 +101,7 @@ class Matkul:
         print self.nama, "\t", domain.ptrRuangan.nama, "\t", stringHari, "\t", domain.jamMulai, "-", domain.jamSelesai
 
 class Domain:
+    #hanya butuh pointer ke Ruangan, karena objek dia sendiri sudah dipegang oleh class Matkul di dalam listDomain
     def __init__(self, ptrRuangan, hari, jamMulai, jamSelesai):
         self.ptrRuangan = ptrRuangan #Ruangan yang ditempati
         self.hari = int(hari)
@@ -103,7 +113,7 @@ def bacaTestcase(namaFile):
     fileTestcase = open(namaFile, "r")
     #parsing
     for stringBuffer in fileTestcase:
-        line = stringBuffer.split("\n")[0]
+        line = stringBuffer.split("\n")[0] #menghilangkan ENTER di karakter terakhir
         if line == "Ruangan": #bagian Ruangan
             status = "r"
         elif line == "Jadwal": #bagian Jadwal
@@ -111,14 +121,14 @@ def bacaTestcase(namaFile):
         elif line == "": #pemisah antar bagian
             continue
         elif status == "r":
-            parsed = line.split(";")
+            parsed = line.split(";") #string nama, int jamBuka, int jamTutup, int[] hari
             newObjekRuangan = Ruangan(parsed[0], parsed[1], parsed[2], parsed[3].split(","))
             listRuangan.append(newObjekRuangan) #daftarkan objek baru ke list
         elif status == "j":
-            parsed = line.split(";")
+            parsed = line.split(";") #string nama, string constraint ruang, int jamBuka, int jamTutup, int sks, int[] hari
             newObjekMatkul = Matkul(parsed[0], parsed[2], parsed[3], parsed[4], parsed[5].split(","))
-            domain = makeListDomain(newObjekMatkul, parsed[1]) #cari domain mana saja yang memungkinkan
-            newObjekMatkul.addListDomain(domain)
+            domain = makeListDomain(newObjekMatkul, parsed[1]) #cari tempat dan waktu mana saja yang memungkinkan
+            newObjekMatkul.addListDomain(domain) #daftarkan domain-domain tersebut ke listDomain
             listMatkul.append(newObjekMatkul) #daftarkan objek baru ke list
 
 def makeListDomain(matkul, consRuangan):
@@ -130,12 +140,12 @@ def makeListDomain(matkul, consRuangan):
             for hariRuangan in ruang.hari:
                 for hariMatkul in matkul.hari:
                     if hariRuangan == hariMatkul:
-                        #cari jam yang cocok, mainkan batasan jam yang dibutuhkan Matkul dengan batasan jam yang tersedia di Ruangan pada hari ini
+                        #lihat jam yang tersedia oleh Ruangan dengan yang dibutuhkan Matkul (cari jam yang cocok)
                         jamMulai = matkul.jamBuka
                         jamSelesai = jamMulai + matkul.sks
                         while jamSelesai <= ruang.jamTutup and jamSelesai <= matkul.jamTutup:
                             if jamMulai >= ruang.jamBuka:
-                                #dapat 1 kandidat Matkul BISA ditempatkan di Ruangan ini, di hari ini, dan di jam segini
+                                #dapat 1 kandidat: Matkul BISA ditempatkan di Ruangan ini, di hari ini, dan di jam segini
                                 newObjekDomain = Domain(ruang, hariRuangan, jamMulai, jamSelesai)
                                 hasil.append(newObjekDomain)
                             jamMulai += 1
@@ -144,12 +154,11 @@ def makeListDomain(matkul, consRuangan):
 
 def initializeRandom():
     for matkul in listMatkul:
-        matkul.setIdxDomain(random.randint(0, matkul.nDomain - 1))
-        #heuristik = taruh matkul sepagi mungkin
+        matkul.setIdxDomain(random.randint(0, matkul.nDomain - 1)) #pure random
+        #heuristik = idxPlus terus menerus sampai menemukan jam paling pagi di hari esoknya
         domainNow = matkul.getDomain()
         matkul.idxPlus()
         domainNew = matkul.getDomain()
-        #lakukan idxPlus terus menerus sampai menemukan jam paling pagi di hari esoknya
         while(domainNew.jamMulai > domainNow.jamMulai):
             domainNow = domainNew
             matkul.idxPlus()
@@ -158,10 +167,10 @@ def initializeRandom():
 def countConflicts():
     del listKonflik[:] #list yang di program utama di hapus dulu karena mau di generate ulang
     hasil = 0 #banyaknya konflik
-    listKonflikLokal = [] #list konflik lokal (beda dari yang di program utama)
+    listKonflikLokal = [] #list konflik lokal (beda dari yang di program utama) supaya pointer tidak kemana mana
     #cari konflik per Ruangan
     for ruangan in listRuangan:
-        #cek di setiap slot time nya
+        #cek di setiap sel nya
         for i in range(11):
             for j in range(5):
                 a_sel = ruangan.sel[i][j]
@@ -182,41 +191,41 @@ def countConflicts():
 def hillOrStimulated(tempMax, tempMin, threshold, decrease):
     lokalMaks = False #kalo terjebak di lokal maks, bernilai true
     step = 0 #sudah berapa kali iterasi
-    listKonflikNow = [] #list konflik lokal (beda dari yang di program utama)
+    listKonflikLokal = [] #list konflik lokal (beda dari yang di program utama)
     nKonflikNow = countConflicts()
-    #mulai hill climbing, iterasi dibatasi  sebanyak step
-    while nKonflikNow > 0:
-        #copy list supaya gak ribet pointer-pointer nya berubah
-        del listKonflikNow[:]
+    #mulai hill climbing, iterasi tidak dibatasi (sampai dapat solusi atau sampai lokal maksimum)
+    while nKonflikNow > 0 and lokalMaks == False:
+        #copy list konflik di program utama ke list konflik lokal (agar pointer tidak berantakan)
+        del listKonflikLokal[:]
         for matkul in listKonflik:
-            listKonflikNow.append(matkul)
-        #heuristik = geser matkul yang konflik menjadi tepat setelah matkul lawannya (tidak overlap lagi)
+            listKonflikLokal.append(matkul)
+        #heuristik = geser matkul yang konflik menjadi tepat setelah matkul lawannya selesai (tidak overlap lagi)
         foundBetter = False
-        for matkul in listKonflikNow:
-            #majuin terus sebanyak nDomain kali (1 cycle, kasus terburuk, matkul ini gabisa diapa2in lagi)
+        for matkul in listKonflikLokal:
+            #majuin terus sebanyak nDomain kali (kasus terburuk yaitu semua domain dicoba dan gaada yang lebih baik)
             for i in range(matkul.nDomain):
-                step += 1
-                tempMax -= decrease
-                matkul.idxPlus() #saat dimajuin, listKonflik berubah namun listKonflikNow tetap
-                nKonflikNew = countConflicts()
-                if nKonflikNew < nKonflikNow: #bandingkan konflik
+                step += 1 #iterasi bertambah
+                tempMax -= decrease #toleransi berkurang
+                matkul.idxPlus()
+                nKonflikNew = countConflicts() #prosedur ini merubah list konflik program utama saja (lokal tetap aman)
+                if nKonflikNew < nKonflikNow: #ternyata domain setelah dimajuin 1 jadi lebih baik
                     foundBetter = True
                     break #break for i
-                else: #nKonflikNew >= nKonflikNow
-                    if tempMax > threshold: #masih mungkin random
+                else: #sama aja atau bahkan lebih buruk, cek apakah bisa ditolerir untuk tetap diambil
+                    if tempMax > threshold:
                         hasilRandom = random.randint(tempMin, tempMax);
                         if hasilRandom >= threshold: #ambil walaupun lebih banyak konflik
                             foundBetter = True
                             break #break for i
-            nKonflikNow = nKonflikNew #berhasil atau tidak menemukan yang lebih baik, tetap ambil nilainya
+            nKonflikNow = nKonflikNew
             if foundBetter == True:
-                break #break for matkul
-            elif matkul == listKonflikNow[len(listKonflikNow) - 1]:
-                lokalMaks = True #matkul terakhir, tetep gak nemu lebih baik
-        if lokalMaks == True: #gak bisa ngapa2in lagi udah mentok
-            print "    LOKAL MAKSIMUM"
-            break #break while
-    #sudah iterasi sebanyak step, atau sudah menemukan solusi
+                break #break for matkul, gausah geser matkul lain juga
+            elif matkul == listKonflikLokal[len(listKonflikLokal) - 1]:
+                lokalMaks = True #semua domain dari semua matkul yang konflik sudah dicoba, gaada yang lebih baik
+                print "    LOKAL MAKSIMUM"
+        #keterangan tambahan : yang dicoba diganti domainnya HANYA matkul yg konflik,
+        #sedangkan matkul lain yang sudah fit pada selnya tidak di ubah
+    #hasil : dapat solusi atau terjebak lokal maksimum
     if nKonflikNow == 0:
         print "SOLUSI DITEMUKAN DALAM", step, "ITERASI"
     else:
@@ -236,35 +245,36 @@ def geneticAlgorithm():
         #hitung fitness tiap gen
         del fitness[:]
         for i in range(len(listGen)):
-            for ruang in listRuangan:
-                ruang.deleteAllSel()
+            #pasangkan dulu matkul dengan domain sesuai dengan yang ada pada gen, agar sel di tiap Ruangan sesuai
+            restart()
             for j in range(len(listGen[i])):
                 listMatkul[j].setIdxDomain(listGen[i][j])
+            #hitung total sel yang tidak konflik, ini fitnessnya
             fitness.append(0)
             for ruang in listRuangan:
-                fitness[i] += ruang.countFitness() #total sel yang tidak konflik
+                fitness[i] += ruang.countFitness()
             if fitness[i] == fitnessMax:
-                print "SOLUSI DITEMUKAN DALAM", keturunan, "GENERASI:"
+                print "SOLUSI DITEMUKAN DALAM GENERASI", keturunan
         #cari gen terjelek dan terbagus
         idxMin = fitness.index(min(fitness))
         idxMax = fitness.index(max(fitness))
         keturunan += 1
-        if keturunan == 10:
-            #udah sekian kali kawin silang dan gak nemu, keluarin aja fitness yang terbaik
-            del listKonflik[:]
-            for ruang in listRuangan:
-                ruang.deleteAllSel()
+        #generasi lebih dari 5 menyebabkan kembar semua (tidak dapat hasil signifikan)
+        if keturunan == 5:
+            #pasangkan lagi matkul dengan domain kepunyaan gen terbaik (fitness terbesar)
+            restart()
             for i in range(len(listGen[idxMax])):
                 listMatkul[i].setIdxDomain(listGen[idxMax][i])
-            print countConflicts(), "KONFLIK DALAM", keturunan, "GENERASI:"
+            #hitung konflik
+            print countConflicts(), "KONFLIK DALAM GENERASI", keturunan
             for matkul in listKonflik:
                 print " ", matkul.nama
             return
         else:
-            #paling jelek timpa dengan paling bagus
+            #gen jelek timpa dengan gen bagus
             for i in range(len(listMatkul)):
                 listGen[idxMin][i] = listGen[idxMax][i]
-            #kawin silang tanpa mutasi
+            #kawin silang TANPA mutasi, mulai dari idxBelah sampai index terakhir
             idxBelah = random.randint(1, len(listMatkul) - 2)
             for i in range(idxBelah, len(listMatkul)):
                 #swap idx 0 dan 1
@@ -276,13 +286,12 @@ def geneticAlgorithm():
                 listGen[2][i] = listGen[3][i]
                 listGen[3][i] = temp
 
-def restart():
+def restart(): #hapus data agar bisa dipakai ulang
     del listKonflik[:]
     for ruang in listRuangan:
         ruang.deleteAllSel()
-    initializeRandom()
 
-def printHasil():
+def printHasil(): #gaperlu dimengerti, hiasan print doang
     print "MATKUL\tRUANG\tHARI\tPUKUL"
     for matkul in listMatkul:
         matkul.printConsole()
@@ -293,25 +302,36 @@ def printHasil():
         selTerisi += ruang.countFilledSel()
     print "Persentasi keefektifan =", "%.2f" % (selTerisi / totalSel * 100), "persen.\n"
 
+def execHC():
+    print "====HILL CLIMBING===="
+    restart()
+    initializeRandom()
+    hillOrStimulated(1, 1, 5, 1) #atur temperatur menjadi low
+    printHasil()
+
+def execSA():
+    print "====STIMULATED ANNEALING===="
+    restart()
+    initializeRandom()
+    hillOrStimulated(100, 1, 5, 1)
+    printHasil()
+
+def execGA():
+    print "====GENETIC ALGORITHM (4 GENES)===="
+    for gen in listGen:
+        del gen[:]
+        #initializeRandom() tanpa heuristik
+        for matkul in listMatkul:
+            gen.append(random.randint(0, matkul.nDomain - 1)); #masukkan ke gen idxDomain nya saja
+    geneticAlgorithm()
+    printHasil()
+
 #PROGRAM UTAMA
 listRuangan = []
 listMatkul = []
 listKonflik = []
-listGen = [[], [], [], []] #list of list of idxDomain untuk GA only
+listGen = [[], [], [], []] #list of list of idxDomain for GA only
 bacaTestcase("Testcase.txt")
-print "====HILL CLIMBING===="
-restart()
-hillOrStimulated(1, 1, 5, 1) #atur temperatur menjadi low
-printHasil()
-print "====STIMULATED ANNEALING===="
-restart()
-hillOrStimulated(100, 1, 5, 1)
-printHasil()
-print "====GENETIC ALGORITHM (4 GENES)===="
-for gen in listGen:
-    del gen[:]
-    #initializeRandom() tanpa heuristik
-    for matkul in listMatkul:
-        gen.append(random.randint(0, matkul.nDomain - 1)); #masukkan ke gen idxDomain nya saja
-geneticAlgorithm()
-printHasil()
+execHC()
+execSA()
+execGA()
