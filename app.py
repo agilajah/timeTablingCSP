04df1cd305b11421b661c0733a73654a78d78f44
agilajah <febiagil20@gmail.com>
2016-10-03 19:42:59 +0700

    hampir jadi

---
+++ app.py
@@ -0,0 +1,79 @@
+import os
+
+import algorithmz
+from flask import Flask, render_template, json, request, redirect, url_for, jsonify
+from werkzeug.wsgi import LimitedStream
+import uuid
+
+
+app = Flask(__name__)
+
+class StreamConsumingMiddleware(object):
+
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        stream = LimitedStream(environ['wsgi.input'],
+                               int(environ['CONTENT_LENGTH'] or 0))
+        environ['wsgi.input'] = stream
+        app_iter = self.app(environ, start_response)
+        try:
+            stream.exhaust()
+            for event in app_iter:
+                yield event
+        finally:
+            if hasattr(app_iter, 'close'):
+                app_iter.close()
+
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app.wsgi_app = StreamConsumingMiddleware(app.wsgi_app)
+
+@app.route("/")
+@app.route("/index")
+def main():
+    return render_template('index.html')
+
+@app.route("/timeTabling")
+def timeTabling():
+    return render_template('timeTabling.html')
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        file = request.files['file']
+        extension = os.path.splitext(file.filename)[1]
+        f_name = str(uuid.uuid4()) + extension
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_name))
+        return json.dumps({'filename':f_name})
+
+@app.route("/timeTabling/result", methods=['POST'])
+def findSchedule():
+
+    # Initialize the errors variable to empty string. We will have the error messages
+    # in that variable, if any.
+    errors = ''
+    check = request.form.get('file')
+    try:
+            if request.form.get('file') is None:
+                _filePath = ''
+                return render_template('timeTabling.html')
+            else:
+                _filePath = '/home/febiagil/Workspaces/workspace_python/tubes_ai/uploads/' + request.form.get('file')
+                algorithmz.bacaTestcase(_filePath)
+                if (request.form.get('algooptions') == "simulatedannealing"):
+                    algorithmz.execSA()
+                if (request.form.get('algooptions') == "hillclimbing"):
+                    algorithmz.execHC()
+                if (request.form.get('algooptions') == "geneticalgorithm"):
+                    algorithmz.execSA()
+
+                return render_template('result.html', listRuangan=algorithmz.listRuangan, listMatkul=algorithmz.listMatkul, listKonflik=algorithmz.listKonflik)
+    except Exception as e:
+        return render_template('404.html',error = str(e), check=check)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='timetabling', version='1.0',
+      description='Code example demonstrating how to upload a file using Flask',
+      author='Febi Agil Ifdillah', author_email='febiagil20@gmail.com',
+      url='http://127.0.0.1:5000/',
+
+      #  Uncomment one or more lines below in the install_requires section
+      #  for the specific client drivers/modules your application needs.
+      install_requires=['flask']
+     )
